c4727dbc199f89fb44b3de90026f35fd5f2db9d4
Omar Hany <OmarMohamed20150362@fci.helwan.edu.eg>
2025-04-26 10:59:59 +0300

    Session 12

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -8,12 +8,4 @@ public class HomePage extends MethodHandles {
     public HomePage(WebDriver driver) {
         super(driver);
     }
-
-    private final By signUpAndLogin = By.linkText("Signup / Login");
-
-
-    public LoginPage clickOnSignUpAndLoginButton(){
-        click(signUpAndLogin,5);
-        return new LoginPage(driver);
-    }
 }
--- src/main/java/utils/MethodHandles.java
+++ src/main/java/utils/MethodHandles.java
@@ -4,11 +4,10 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -22,6 +21,9 @@ public class MethodHandles {
     static ExtentTest test;
 
     protected WebDriver driver;
+    Select select;
+
+    Actions actions;
 
     WebDriverWait wait;
 
@@ -29,10 +31,11 @@ public class MethodHandles {
         this.driver = driver;
     }
 
-    private WebElement webElement(By locator){
+    private WebElement webElement(By locator) {
         return driver.findElement(locator);
     }
-    private void explicitWait(int time, By locator){
+
+    private void explicitWait(int time, By locator) {
         wait = new WebDriverWait(driver, Duration.ofSeconds(time));
         wait.until(ExpectedConditions.and(
                 ExpectedConditions.visibilityOf(webElement(locator)),
@@ -42,20 +45,314 @@ public class MethodHandles {
         ));
     }
 
-    protected void click(By locator, int time){
-        explicitWait(time,locator);
-        webElement(locator).click();
-        setSteps();
-        addBorderToElement(driver, webElement(locator));
+    protected void click(By locator, int time) {
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                webElement(locator).click();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+    }
+
+    protected void selectByIndex(By locator, int time, int index) {
+        select = new Select(webElement(locator));
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                select.selectByIndex(index);
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+    }
+
+    protected void selectByValue(By locator, int time, String value) {
+        select = new Select(webElement(locator));
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                select.selectByValue(value);
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+    }
+
+    protected void selectByVisibleText(By locator, int time, String visibleText) {
+        select = new Select(webElement(locator));
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                select.selectByVisibleText(visibleText);
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+    }
+
+    protected void submit(By locator, int time) {
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                webElement(locator).submit();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+    }
+
+    protected void clear(By locator, int time) {
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                webElement(locator).clear();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+    }
+
+    protected boolean isSelected(By locator, int time) {
+        boolean flag = false;
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                flag = webElement(locator).isSelected();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+        return flag;
+    }
+
+    protected boolean isEnabled(By locator, int time) {
+        boolean flag = false;
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                flag = webElement(locator).isEnabled();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+        return flag;
+    }
+
+    protected boolean isDisplayed(By locator, int time) {
+        boolean flag = false;
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                flag = webElement(locator).isDisplayed();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+        return flag;
+    }
+
+    protected String getText(By locator, int time) {
+        String text = null;
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                text = webElement(locator).getText();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+        return text;
+    }
+
+    protected void acceptAlert() {
+        driver.switchTo().alert().accept();
+    }
+
+    protected void dismissAlert() {
+        driver.switchTo().alert().dismiss();
+    }
+
+    protected void sendKeysAlert(String text) {
+        driver.switchTo().alert().sendKeys(text);
+    }
+
+    protected String getTextAlert() {
+        return driver.switchTo().alert().getText();
+    }
+
+    protected void switchToFrame(String nameOrId) {
+        driver.switchTo().frame(nameOrId);
+    }
+
+    protected void switchToFrame(By locator) {
+        driver.switchTo().frame(webElement(locator));
+    }
+
+    protected void switchToParent() {
+        driver.switchTo().parentFrame();
+    }
+
+
+    protected void sendKeys(By locator, int time, String text) {
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                webElement(locator).sendKeys(text);
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
 
     }
 
-    protected void sendKeys(By locator, int time, String text){
-        explicitWait(time,locator);
-        webElement(locator).sendKeys(text);
-        setSteps();
-        addBorderToElement(driver, webElement(locator));
+    protected void clickWithActions(By locator, int time) {
+        actions = new Actions(driver);
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                actions.click(webElement(locator)).build().perform();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+
     }
+
+    protected void doubleClick(By locator, int time) {
+        actions = new Actions(driver);
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                actions.doubleClick(webElement(locator)).build().perform();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+
+    }
+
+    protected void contextClick(By locator, int time) {
+        actions = new Actions(driver);
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                actions.contextClick(webElement(locator)).build().perform();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+
+    }
+    protected void moveToElement(By locator, int time) {
+        actions = new Actions(driver);
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                actions.moveToElement(webElement(locator)).build().perform();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+
+    }
+    protected void clickAndHold(By locator, int time) {
+        actions = new Actions(driver);
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, locator);
+                actions.clickAndHold(webElement(locator)).build().perform();
+                setSteps();
+                addBorderToElement(driver, webElement(locator));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+
+    }
+    protected void dragAndDrop(By src, By target, int time) {
+        actions = new Actions(driver);
+        for (int i = 0; i < 5; i++) {
+            try {
+                explicitWait(time, src);
+                explicitWait(time, target);
+                actions.dragAndDrop(webElement(src), webElement(target)).build().perform();
+                setSteps();
+                addBorderToElement(driver, webElement(src));
+                addBorderToElement(driver, webElement(target));
+                break;
+            } catch (StaleElementReferenceException e) {
+
+            }
+        }
+
+    }
+
+
+    protected void contextClick() {
+        actions = new Actions(driver);
+        actions.contextClick().build().perform();
+        setSteps();
+    }
+    protected void release() {
+        actions = new Actions(driver);
+        actions.release().build().perform();
+        setSteps();
+    }
+
+    protected void clickWithActions() {
+        actions = new Actions(driver);
+        actions.click().build().perform();
+        setSteps();
+    }
+
 
     private static String getMethodName() {
         StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
@@ -67,11 +364,12 @@ public class MethodHandles {
             return "Unknown";
         }
     }
-    public void setSteps(){
+
+    public void setSteps() {
         test.info(getMethodName());
     }
 
-    public static void myAssertEquals(Object actual, Object expected){
+    public static void myAssertEquals(Object actual, Object expected) {
         test.info(MarkupHelper.createLabel("----------------- Actual Result -----------------", ExtentColor.BLUE));
         test.info(actual.toString());
 
@@ -80,6 +378,7 @@ public class MethodHandles {
 
         assertEquals(actual, expected);
     }
+
     private static void addBorderToElement(WebDriver driver, WebElement element) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].style.border = '5px solid red';", element);
