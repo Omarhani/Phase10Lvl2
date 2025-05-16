be64babcce84ab8d7d5aa37617d3342be0ddb7f6
Esraa Sherief <esraasherief@gmail.com>
2025-05-17 00:23:32 +0300

    Test case7 : Confirm testcase Exist

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -8,4 +8,16 @@ public class HomePage extends MethodHandles {
     public HomePage(WebDriver driver) {
         super(driver);
     }
+
+
+    By TestCaseButton =By.xpath("//*[contains(text(),'Test Cases')]");
+    By TestCaseTitle = By.xpath("//h2/b");
+
+    public void assertTestCasesExists() {
+        myAssertEquals(getText(TestCaseButton, 2), "Test Cases");
+        click(TestCaseButton, 5);
+        myAssertEquals(getText(TestCaseTitle, 2), "TEST CASES");
+    }
+
+
 }
