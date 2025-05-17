1bf4fc34b014c19ab9dcd13db485d547cef7c420
Esraa Sherief <esraasherief@gmail.com>
2025-05-17 22:25:16 +0300

    Update locator Create Class TestcasesTests

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -11,12 +11,12 @@ public class HomePage extends MethodHandles {
 
 
     By TestCaseButton =By.xpath("//*[contains(text(),'Test Cases')]");
-    By TestCaseTitle = By.xpath("//h2/b");
+    By TestCaseTitle = By.xpath("//*[contains(text(),'Below is the list of test Cases for you to practice the Automation. Click on the scenario for detailed Test Steps:')]");
 
     public void assertTestCasesExists() {
         myAssertEquals(getText(TestCaseButton, 2), "Test Cases");
         click(TestCaseButton, 5);
-        myAssertEquals(getText(TestCaseTitle, 2), "TEST CASES");
+        myAssertEquals(getText(TestCaseTitle, 2), "Below is the list of test Cases for you to practice the Automation. Click on the scenario for detailed Test Steps:");
     }
 
 
